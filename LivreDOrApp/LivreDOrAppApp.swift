572b4cefa208079d13ebff2f10cec0c1fe7d115b
nydina <63366733+nydina@users.noreply.github.com>
2022-10-24 14:49:56 +0200

    Initial Commit

---
+++ LivreDOrApp/LivreDOrAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LivreDOrAppApp.swift
+//  LivreDOrApp
+//
+//  Created by Dina Andrianarijaona on 24/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct LivreDOrAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
